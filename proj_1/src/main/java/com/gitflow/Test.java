5b9fb6b96e8d8cc72dd59d3dc0431081aaf8d345
German Barros <gbarros@3cinteractive.com>
2013-12-13 12:09:47 -0200

    sdf

--- proj_1/src/main/java/com/gitflow/Test.java
+++ proj_1/src/main/java/com/gitflow/Test.java
@@ -9,7 +9,7 @@ public class Test {
 
 
 		System.out.println("1");
-		System.out.println("cambio en release 1");
+		System.out.println("cambio en rasdasdelease 1");
 		System.out.println("feature-1");
 	}
 }
